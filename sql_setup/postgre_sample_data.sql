dfc28e8e70ead49ced34cc928bcd6d7c44c04952
Sam Berven <samberven@gmail.com>
2024-10-19 03:33:47 -0700

    Implement Supabase connection, add sample Users data, implement (working) get users route

---
+++ sql_setup/postgre_sample_data.sql
@@ -0,0 +1,22 @@
+-- Insert starter user data
+INSERT INTO users (name, registration_date)
+VALUES
+('Sam', NOW()),
+('Paola', NOW()),
+('Liz', NOW());
+
+-- Insert starter badges data
+
+-- Insert difficulty data. This table is not modifiable.
+
+-- Insert starter challenges data
+
+-- Insert starter goals data
+
+-- Insert starter tags data
+
+-- Insert starter challenge_goals data
+
+-- Insert starter challenge_tags data
+
+-- Insert starter users_badges data
--- test_backend/src/server.ts
+++ test_backend/src/server.ts
@@ -1,10 +1,29 @@
 import express from "express";
+import supabase from "./config/supabaseClient";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Backend server test!");
+  res.send("The backend server is running!");
+});
+
+// Returns all users
+app.get("/api/users", async (req, res) => {
+  try {
+    const { data, error } = await supabase
+      .from("users") // Replace with your table name
+      .select("*");
+
+    if (error) {
+      throw error;
+    }
+
+    res.status(200).json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error-- Unable to fetch "users" data.' });
+  }
 });
 
 app.listen(PORT, () => {
