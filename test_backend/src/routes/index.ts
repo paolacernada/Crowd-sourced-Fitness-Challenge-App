2d3830f4f4c29ec68c6baf067bb0f1562eb383bb
Sam Berven <samberven@gmail.com>
2024-10-20 19:54:31 -0700

    Add get difficulties route

--- test_backend/src/routes/index.ts
+++ test_backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRoutes from "./usersRoutes";
+import difficultyRoutes from "./difficultyRoutes";
 
 const router = Router();
 
 router.use("/users", userRoutes); // Prefixes all routes in userRoutes.ts with 'users'
+router.use("/difficulties", difficultyRoutes); // Prefixes all routes in difficultyRoutes.ts with 'difficulties'
 
 export default router;
