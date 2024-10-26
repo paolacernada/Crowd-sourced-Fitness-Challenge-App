27031d0538c8c836745492b6b60f0fa9ba36fc53
Sam Berven <samberven@email.com>
2024-10-25 23:55:37 -0700

    Implement challenges CRU, and goals CR routes. Still need to add checks to Update routes

--- sql_setup/postgre_sample_data.sql
+++ sql_setup/postgre_sample_data.sql
@@ -15,8 +15,19 @@ VALUES
 ('Hard');
 
 -- Insert starter challenges data
+INSERT INTO challenges (name, description, difficulty)
+VALUES
+('Run a mile', 'Go from zero to able to run a mile', 'Easy'),
+('Run a 5k', 'Go from a mile to a 5k', 'Easy'),
+('Run a half-marathon', 'Go from a 5k to a half-marathon', 'Medium'),
+('Run a marathon', 'Go from a half-marathon to a full marathon', 'Hard')
 
 -- Insert starter goals data
+INSERT INTO goals (name, description)
+VALUES
+    ('5 times', 'Participate 5 times'),
+    ('10 times', 'Participate 10 times'),
+    ('15 times', 'Participate 15 times')
 
 -- Insert starter tags data
 
