b1503994cc1e82ab5de9504549f6e5c8290d5aa2
Elizabeth Ward <emward@Lizs-M3.local>
2024-11-01 23:31:23 -0400

    Implemented User Profile screen

--- react_native_frontend/app/profile_screen.tsx
+++ react_native_frontend/app/profile_screen.tsx
@@ -14,20 +14,18 @@ interface Profile {
   id: number;
   name: string;
   registration_date: string;
-  email: string | null;  
 }
 
 export default function ProfileScreen() {
   const [userProfile, setUserProfile] = useState<Profile | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  const { userId = "2"} = useLocalSearchParams();
+  const { userId } = useLocalSearchParams();
   const { theme } = useTheme();
-
 
   useEffect(() => {
     const loadUserProfile = async () => {
       if (!userId) return;
-  
+
       try {
         const userIdNumber = parseInt(userId as string, 10);
         const profile = await fetchUserProfile(userIdNumber);
@@ -42,21 +40,47 @@ export default function ProfileScreen() {
 
   if (loading) {
     return (
-      <ActivityIndicator size="large" color={theme === "dark" ? "#fff" : "#000"} />
+      <ActivityIndicator
+        size="large"
+        color={theme === "dark" ? "#fff" : "#000"}
+      />
     );
   }
 
   if (!userProfile) {
-    return <Text style={{ color: theme === "dark" ? "#fff" : "#000" }}>No user profile found.</Text>;
+    return;
+    <Text style={{ color: theme === "dark" ? "#fff" : "#000" }}>
+      No user profile found.
+    </Text>;
   }
 
   return (
-    <View style={[styles.container, theme === "dark" ? styles.darkContainer : styles.lightContainer]}>
-      <Text style={[styles.appName, theme === "dark" ? styles.darkAppName : styles.lightAppName]}>
+    <View
+      style={[
+        styles.container,
+        theme === "dark" ? styles.darkContainer : styles.lightContainer,
+      ]}
+    >
+      <Text
+        style={[
+          styles.appName,
+          theme === "dark" ? styles.darkAppName : styles.lightAppName,
+        ]}
+      >
         FitTogether Challenges
       </Text>
-      <View style={[styles.formContainer, theme === "dark" ? styles.darkForm : styles.lightForm]}>
-        <Text style={[styles.title, theme === "dark" ? styles.darkText : styles.lightText]}>
+      <View
+        style={[
+          styles.formContainer,
+          theme === "dark" ? styles.darkForm : styles.lightForm,
+        ]}
+      >
+        <Text
+          style={[
+            styles.title,
+            theme === "dark" ? styles.darkText : styles.lightText,
+          ]}
+        >
           User Profile
         </Text>
         <Text style={theme === "dark" ? styles.darkText : styles.lightText}>
@@ -68,7 +92,12 @@ export default function ProfileScreen() {
         <Text style={theme === "dark" ? styles.darkText : styles.lightText}>
           Created At: {userProfile.registration_date}
         </Text>
-        <TouchableOpacity style={[styles.button, theme === "dark" ? styles.darkButton : styles.lightButton]}>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            theme === "dark" ? styles.darkButton : styles.lightButton,
+          ]}
+        >
           <Text style={styles.buttonText}>Edit Profile</Text>
         </TouchableOpacity>
       </View>
--- react_native_frontend/src/services/userService.tsx
+++ react_native_frontend/src/services/userService.tsx
@@ -2,17 +2,17 @@
 
 export const fetchUserProfile = async (userId: number) => {
   const response = await fetch(`http://localhost:3000/api/users/${userId}`, {
-    headers: { 'Content-Type': 'application/json' }
+    headers: { "Content-Type": "application/json" },
   });
-  
+
   if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
   }
-  
+
   try {
     const data = await response.json();
     return data;
   } catch (error) {
     return null;
   }
-}
+};
