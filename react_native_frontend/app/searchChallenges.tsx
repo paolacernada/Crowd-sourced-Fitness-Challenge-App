dc4627a6474c588c1da115c17de3233d5382a8ca
Paola Cernada <96987381+paolacernada@users.noreply.github.com>
2024-11-15 02:25:50 -0800

    feat: implement ChallengeDetailsScreen to display challenge details

---
+++ react_native_frontend/app/searchChallenges.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import SearchChallengesScreen from "../src/screens/SearchChallengesScreen";
+
+export default function SearchChallenges() {
+  return (
+    <View style={{ flex: 1 }}>
+      <SearchChallengesScreen />
+    </View>
+  );
+}
