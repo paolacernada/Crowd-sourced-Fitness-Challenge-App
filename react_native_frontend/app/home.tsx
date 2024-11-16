96b54cd7b5b1375d37f3e204d54d5dae5955f328
Sam Berven <samberven@email.com>
2024-11-16 00:08:55 -0800

    Rename searchChallenges work (for us as a backup),

--- react_native_frontend/app/home.tsx
+++ react_native_frontend/app/home.tsx
@@ -81,7 +81,7 @@ export default function HomeScreen() {
             theme === "dark" ? styles.darkButton : styles.lightButton,
             { width: "70%" },
           ]}
-          onPress={() => router.push("/searchChallenges")}
+          onPress={() => router.push("/searchChallengesSAM")}
           disabled={loading}
         >
           <Text style={styles.buttonText}>
